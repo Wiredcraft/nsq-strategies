e5a107b8667e458986a4ce30f69587600515034f
Xavier Zhou <xiayezhou@gmail.com>
2018-12-30 00:57:23 +0800

    chore: fix test code

--- test/producer.test.js
+++ test/producer.test.js
@@ -279,17 +279,12 @@ describe('producer', () => {
             ]).on('close', (code) => {
               startNsqd((e) => {
                 setTimeout(() => { //wait to reconnection
-                  p.connect((e) => {
-                    if (e) {
-                      return done(e);
-                    }
-                    p.produce(TOPIC, 'message after reconnect', (error) => {
-                      expect(error).to.not.exist;
-                      spawn('docker-compose', [
-                        `--file=${composeFile}`, 'stop', 'nsqd3'
-                      ]);
-                      done();
-                    });
+                  p.produce(TOPIC, 'message after reconnect', (error) => {
+                    expect(error).to.not.exist;
+                    spawn('docker-compose', [
+                      `--file=${composeFile}`, 'stop', 'nsqd3'
+                    ]);
+                    done();
                   });
                 }, 6000); //1st reconnect after 1 sec, then 2 sec later
               });
