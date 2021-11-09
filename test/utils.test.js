1eea9c286508d405317f3577ee94b616133a11b1
Xavier Zhou <xiayezhou@gmail.com>
2021-11-09 11:10:12 +0800

    fix: avoid index out of bounds issue

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,12 +2,12 @@
 
 const expect = require('chai').expect;
 
-const { pickSomeWithCursor } = require('../lib/utils');
+const { partialPickWithIndex } = require('../lib/utils');
 
-describe('pickSomeWithCursor', () => {
+describe('partialPickWithIndex', () => {
   it('should get N', () => {
     const arr = ['foo', 'bar', 'baz', 'quz'];
-    const result = pickSomeWithCursor(2, 0, arr);
+    const result = partialPickWithIndex(2, 0, arr);
     expect(result).to.have.lengthOf(2);
     expect(result[0]).to.equal('foo');
     expect(result[1]).to.equal('bar');
@@ -15,14 +15,24 @@ describe('pickSomeWithCursor', () => {
 
   it('should get N even if the cursor + N exceeds the length', () => {
     const arr = ['foo', 'bar', 'baz', 'quz'];
-    const result = pickSomeWithCursor(2, 3, arr);
+    const result = partialPickWithIndex(2, 3, arr);
     expect(result).to.have.lengthOf(2);
     expect(result[0]).to.equal('quz');
     expect(result[1]).to.equal('foo');
   });
   it('should get all if the number equals to the length', () => {
     const arr = ['foo', 'bar', 'baz', 'quz'];
-    const result = pickSomeWithCursor(4, 1, arr);
+    const result = partialPickWithIndex(4, 1, arr);
+    expect(result).to.have.lengthOf(4);
+    expect(result[0]).to.equal('bar');
+    expect(result[1]).to.equal('baz');
+    expect(result[2]).to.equal('quz');
+    expect(result[3]).to.equal('foo');
+  });
+
+  it('should get all if the number larger than the length', () => {
+    const arr = ['foo', 'bar', 'baz', 'quz'];
+    const result = partialPickWithIndex(6, 1, arr);
     expect(result).to.have.lengthOf(4);
     expect(result[0]).to.equal('bar');
     expect(result[1]).to.equal('baz');
